bb97f0e3de5e61cd8ee42fbd781cd7229815ad7b
M Juma <qasimimohammad819@gmail.com>
2026-05-27 21:45:59 +0200

    component folder creared

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <section>
+      <div>
+        <h1>LOGO</h1>
+        <nav>
+          <ul>
+            <li>HOME</li>
+            <li>SERVICES</li>
+            <li>PROJECTS</li>
+            <li>ABOUT US</li>
+            <li>CONTACT </li>
+          </ul>
+        </nav>
+      </div>
+    </section>
+  );
+};
+
+export default Navbar;
