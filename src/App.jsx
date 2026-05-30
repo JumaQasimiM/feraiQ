4dd09d50f1b7486ee6b0db22fc51dc48bec0cd7c
M Juma <qasimimohammad819@gmail.com>
2026-05-30 22:39:52 +0200

    update App.jsx and App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
 import "./App.css";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar";
 function App() {
   return (
     <>
-      <div className="p-10 bg-slate-800">
-        <h1 className="font-quicksand text-5xl font-extrabold  text-white">
-          title test for firma
-        </h1>
-        <p className="text-slate-400">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-          similique perspiciatis architecto dicta nihil beatae recusandae
-          adipisci maxime aliquam! Dignissimos possimus eos quis animi, iusto
-          earum quisquam cumque ducimus perspiciatis.
-        </p>
-        <button className="bg-slate-500 py-1 px-3 my-3 hover:bg-sky-400 border border-sky-500/20 rounded">
-          Login
-        </button>
-      </div>
+      <BrowserRouter>
+        <Navbar />
+        {/* <Routes>
+          <Route element={} />
+        </Routes> */}
+      </BrowserRouter>
     </>
   );
 }
