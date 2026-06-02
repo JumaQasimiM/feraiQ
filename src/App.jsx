4c2c09ea6058d194d6b723a01094c3de301c8b8c
M Juma <qasimimohammad819@gmail.com>
2026-06-02 10:28:10 +0200

    update: add ITServicesOptions.jsx in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar";
 import Hero from "./components/Hero";
+import ITServiceOptions from "./components/ITServiceOptions";
 // import { Home } from "./components/home";
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Hero />
+        <ITServiceOptions />
         {/* <Home /> */}
         {/* <Routes>
           <Route element={} />
