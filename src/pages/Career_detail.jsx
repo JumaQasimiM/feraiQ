bf42222f255e717f14ddc037eeec92c2fc7fbc4e
M Juma <qasimimohammad819@gmail.com>
2026-06-08 20:21:01 +0200

    update: use usefetch() custom hook to fetch data from api

--- src/pages/Career_detail.jsx
+++ src/pages/Career_detail.jsx
@@ -11,6 +11,7 @@ import { useFetchSingleData } from "../hooks/useFetchSingleData";
 
 // components
 import { Aboutus_careere_detail } from "../components/smallComponents/Aboutus_careere_detail";
+import { TfiEmail } from "react-icons/tfi";
 const Career_detail = () => {
   const { slug } = useParams();
 
@@ -89,59 +90,8 @@ const Career_detail = () => {
             </button>
           </a>
         </div>
+        {/* Sidebar */}
         <Aboutus_careere_detail />
-        {/* Sidebar
-        <aside className="border border-gray-300 rounded relative">
-          <div className=" p-6 space-y-6 ">
-            <div className="space-y-1">
-              <h1 className="font-semibold">Arbeitgeber</h1>
-              <h1>feraiQ GmbH</h1>
-            </div>
-
-            <h3 className="font-semibold mb-3">Über uns</h3>
-
-            <p className="text-sm text-slate-600 leading-relaxed">
-              Seit 2025 unterstützen wir Unternehmen mit professionellen
-              IT-Services und individuellen Lösungen für moderne
-              Geschäftsprozesse.
-            </p>
-            <div>
-              <h1 className="font-semibold">Arbeitsort</h1>
-              <h1 className="flex justify-start items-center gap-2">
-                {" "}
-                <FaLocationDot /> Heilbronner Straße 0, 74000 Heilbronn
-              </h1>
-            </div>
-
-            <div className="mt-4">
-              <h3 className="font-semibold text-lg">Ansprechpartner</h3>
-
-              <p className="font-medium mt-1">Mohammad Juma Qasimi</p>
-
-              <p className="text-slate-600 text-sm">Head of Recruiting</p>
-            </div>
-
-            <div className="mt-5 space-y-2 text-sm">
-              <p className="flex justify-start items-center gap-2">
-                <LuPhone /> +49 1521 479 5072
-              </p>
-              <p className="flex justify-start items-center gap-2">
-                <TfiEmail /> bewerbung@feraiq.de
-              </p>
-            </div>
-
-            <a href="mailto:bewerbung@feraiq.de">
-              <button className="w-full mt-6 bg-sky-600 text-white py-3 rounded-lg hover:bg-sky-700 transition">
-                Jetzt bewerben
-              </button>
-            </a>
-            <Link to={"/careers"}>
-              <button className="w-full mt-6 bg-sky-600 text-white py-3 rounded-lg hover:bg-sky-700 transition">
-                ← Zruck
-              </button>
-            </Link>
-          </div>
-        </aside> */}
       </div>
     </div>
   );
