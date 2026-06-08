e8829ea629cd5793c94ce129b94924505fee2d0a
M Juma <qasimimohammad819@gmail.com>
2026-06-08 21:59:32 +0200

    setup react qury in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,14 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App.jsx";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// react query
+const client = new QueryClient({});
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={client}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 );
