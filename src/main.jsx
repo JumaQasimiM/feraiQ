4e9aa30f5ed863e98e9c6c35e08fd5a1b7752ca2
M Juma <qasimimohammad819@gmail.com>
2026-06-14 10:56:50 +0200

    update: React Query configuration

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,22 @@ import App from "./App.jsx";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-// react query
-const client = new QueryClient({});
+// React Query configuration
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchInterval: 50000,
+      staleTime: 10000, // data is fresh for 10s
+
+      //  Remove inactive query cache after 8 seconds
+      gcTime: 8000, // (when component is inactive)
+      retry: 5,
+      refetchOnMount: false,
+      refetchOnReconnect: true, // when loss the connection with server or refetch when the app reconnects to the network
+    },
+  },
+});
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={client}>
